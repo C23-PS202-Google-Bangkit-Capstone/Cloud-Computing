587ffb83801ece7940a98ebc4461f7b1d42eb70b
Zumoa17 <116241389+Zumoa17@users.noreply.github.com>
2023-06-05 14:29:03 +0700

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./connection');
-const { signupValidation, loginValidation } = require('./validation');
+const db = require('..config/connection');
+const { signupValidation, loginValidation } = require('..middleware/validation');
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
