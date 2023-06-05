e45d3327b3bb0340463eb346192fd12d91d7faf8
Zumoa17 <116241389+Zumoa17@users.noreply.github.com>
2023-06-05 14:11:37 +0700

    Create connection.js

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+//buat koneksi database
+const conn = mysql.createConnection({
+    instanceName: 'freshcheck-c23-ps202f:asia-southeast2:freshcheck-db', // your socket connection path
+    credentials: '',
+    host: '34.101.96.86', // Your ip host
+    user: 'root', // Your username
+    password: 'freshcheck', // Your password
+    database: 'freshcheck-db' // Your database name
+});
+
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log('Mysql terkoneksi');
+});
+
+module.exports = conn;
