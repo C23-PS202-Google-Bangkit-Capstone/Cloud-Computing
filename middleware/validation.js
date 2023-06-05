2013395bec010f0b750fb00b2b85eee9e491516a
Zumoa17 <116241389+Zumoa17@users.noreply.github.com>
2023-06-05 14:12:39 +0700

    Add files via upload

---
+++ middleware/validation.js
@@ -0,0 +1,16 @@
+const { check } = require('express-validator');
+ 
+exports.signupValidation = [
+    check('username', 'username must be 4 or more characters').isLength({ min: 4 }),
+    check('email', 'Please include a valid email').isEmail().normalizeEmail({ gmail_remove_dots: true }),
+    check('password', 'Password must be 6 or more characters').isLength({ min: 6 }),
+    check('phone_number', 'Please include a valid phone number').isLength({ max: 12 }),
+    check('location', 'Location is requied').not().isEmpty()
+]
+ 
+exports.loginValidation = [
+     check('email', 'Please include a valid email').isEmail().normalizeEmail({ gmail_remove_dots: true }),
+     check('password', 'Password must be 6 or more characters').isLength({ min: 6 })
+ 
+]
+
