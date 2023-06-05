cd9f15db11a1c671246b422b306c01137492118f
Zumoa17 <116241389+Zumoa17@users.noreply.github.com>
2023-06-05 14:47:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,37 +4,32 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const Router = require('..routes/router.js');
+const uploadApp = require('..controller/upload');
 
-
-// Bikin instance buat express
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-// set body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 let corsOptions = {
-    origin: "http://localhost:3000",
+  origin: "http://localhost:3000",
 };
 app.use(cors(corsOptions));
 
-// set up router
 app.use('/api', Router);
 
-// Handling Errors
+app.use('/upload', uploadApp);
+
 app.use((err, req, res, next) => {
-    // console.log(err);
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error";
-    res.status(err.statusCode).json({
-      message: err.message,
-    });
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+  res.status(err.statusCode).json({
+    message: err.message,
+  });
 });
 
-// Start Server
-const port = 3000;
 app.listen(port, () => {
-    console.log(`Server berjalan pada ${port}`);
+  console.log(`Server berjalan pada port ${port}`);
 });
