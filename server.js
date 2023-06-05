e9135b0c804634fb991f5a4d1020439c622fa21c
Zumoa17 <116241389+Zumoa17@users.noreply.github.com>
2023-06-05 14:29:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const Router = require('.routes/router.js');
+const Router = require('..routes/router.js');
 
 
 // Bikin instance buat express
