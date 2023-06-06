3c563d03fd4970a357bf669c7f4878e8bc796c63
Frico Simon <74108779+FricoSimon@users.noreply.github.com>
2023-06-06 11:58:29 +0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 //buat koneksi database
 const conn = mysql.createConnection({
     instanceName: 'freshcheck-c23-ps202f:asia-southeast2:freshcheck-db', // your socket connection path
-    credentials: './credentials/user-upload.json',
+    credentials: '/src/app/config/credentials/user-upload.json',
     host: '34.101.96.86', // Your ip host
     user: 'root', // Your username
     password: '', // Your password
